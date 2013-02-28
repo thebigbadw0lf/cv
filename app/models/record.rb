c44c1ce3736f3b22cbebbd3c1d8f25821405bf1d
Alin Maier <alin.maier@gmail.com>
2013-02-28 16:41:24 +0000

    add tagging to records

--- app/models/record.rb
+++ app/models/record.rb
@@ -1,3 +1,4 @@
 class Record < ActiveRecord::Base
-  attr_accessible :company_id, :end_date, :industry_id, :job_title, :link, :start_date, :is_current
+  attr_accessible :company_id, :end_date, :industry_id, :job_title, :link, :start_date, :is_current, :tag_list
+  acts_as_taggable
 end
