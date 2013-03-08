dee03e6f5d6fd7ec5ab992fe4c153755f6fb348a
Alin Maier <alin.maier@gmail.com>
2013-03-08 08:00:10 +0000

    major slider progress

--- app/helpers/cv_helper.rb
+++ app/helpers/cv_helper.rb
@@ -1,2 +1,12 @@
 module CvHelper
+  def year_format(navigation_counter, position, class_past, class_now, class_future)
+    case navigation_counter+1
+      when 0..position-1
+        class_past
+      when position
+        class_now
+      else
+        class_future
+    end
+  end
 end
